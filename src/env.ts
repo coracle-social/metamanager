33eb1efcfddb93966871fae1d7ea996a9bf6f49a
Jon Staab <shtaab@gmail.com>
2025-10-02 15:45:51 -0700

    Move messages to templates, add rejection message

--- src/env.ts
+++ src/env.ts
@@ -15,6 +15,7 @@ if (!process.env.SECRET_KEY) throw new Error('SECRET_KEY is not defined.')
 if (!process.env.ADMIN_ROOM) throw new Error('ADMIN_ROOM is not defined.')
 if (!process.env.ADMIN_RELAY) throw new Error('ADMIN_RELAY is not defined.')
 if (!process.env.ADMIN_PUBKEYS) throw new Error('ADMIN_PUBKEYS is not defined.')
+if (!process.env.RELAY_DOMAIN) throw new Error('RELAY_DOMAIN is not defined.')
 if (!process.env.INDEXER_RELAYS) throw new Error('INDEXER_RELAYS is not defined.')
 
 export const PORT = process.env.PORT
@@ -22,6 +23,7 @@ export const CONFIG_DIR = process.env.CONFIG_DIR
 export const ADMIN_ROOM = process.env.ADMIN_ROOM
 export const ADMIN_RELAY = process.env.ADMIN_RELAY
 export const ADMIN_PUBKEYS = fromCsv(process.env.ADMIN_PUBKEYS)
+export const RELAY_DOMAIN = fromCsv(process.env.RELAY_DOMAIN)
 export const INDEXER_RELAYS = fromCsv(process.env.INDEXER_RELAYS)
 
 export const appSigner = Nip01Signer.fromSecret(process.env.SECRET_KEY)
